8a5db1272c39369b0453c82d51c3da8e5e32c097
Aftab Ahmed <Iffeejaan@users.noreply.github.com>
2017-07-25 04:10:48 -0700

    Add files via upload
    Quiz App

--- app.js
+++ app.js
@@ -1,69 +1,155 @@
-function searchKeyPress(e)
-{
-    // look for window.event in case event isn't passed in
-    e = e || window.event;
-    if (e.keyCode == 13)
+var countdown = 2;
+var current_question = 0;
+var score = 0;
+var wrong = 0;
+var questions = [
     {
-        newElement();
+        'question':'Inside which HTML element do we put the JavaScript?',
+        'options':['javascript','scripting','script','js'],
+        'answer':'script'
+    },
+    {
+        'question':'Where is the correct place to insert a JavaScript?',
+        'options':[
+                 'both the head section and the body section are correct',
+                 'the head section ',
+                 'in the end of body section',
+                 'none of these'
+                 ],
+        'answer':'in the end of body section'
+    },
+    {
+        'question':'Write full abbr for js',
+        'options':['justsure','javascript','johnsmith'],
+        'answer':'javascript'
+    },
+    {
+        'question':'Which operator is used to assign a value to a variable?',
+        'options':['x','-','*','='],
+        'answer':'='
+    },
+    {
+        'question':'Is JavaScript case-sensitive?',
+        'options':['yes','sometimes','no','none'],
+        'answer':'yes'
+    },{
+        'question':'The URL property belongs to which of the following object?',
+        'options':['document ','Element','Location ',' All of the mentioned'],
+        'answer':'document'
+    },
+    {
+        'question':'Javascript is _________ language.',
+        'options':['Programming','Application','None of These','Scripting'],
+        'answer':'Scripting'
+    },
+    {
+        'question':'JavaScript Code is written inside file having extension __________.',
+        'options':['.jvs','.javascript','.js','.jsc'],
+        'answer':'yes'
+    }
+];
+function startNow(){    
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    if (username.length >= 5 && password.length >= 5){
+        document.getElementsByClassName('profile_username')[0].innerText = 'Welcome ' + document.getElementById('username').value;    
+        document.getElementsByClassName('profile_username')[1].innerText = 'Welcome ' + document.getElementById('username').value;    
+        document.getElementById('main_welcome').style = 'display:none';
+        document.getElementById('main_instruction').style = 'display:block';
+    }
+    else {
+        alert('Username/Password length can not be empty and less than 5');
+    }
+
+}
+function startQuiz(){
+    document.getElementById('main_instruction').style = 'display:none';
+    document.getElementById('main').style = 'display:block';
+        // Set the date we're counting down to
+    var countDownDate = new Date();
+    countDownDate.setMinutes(countDownDate.getMinutes() + countdown);
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+    // Get todays date and time
+    var now = new Date().getTime();
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;    
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Output the result in an element with id="demo"
+        document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
+        // If the count down is over, write some text 
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("timer").innerHTML = "EXPIRED";
+            showResult();
+        }
+    }, 1000);
+}
+
+var q_div = document.getElementById('question');
+
+function show_question(current_question){
+    var counter = current_question+1;
+    
+    var qst = '<h4>Question#'+counter+': '+questions[current_question].question+'</h4>';
+    var opt = '<ul>';
+    for(i=0;i<questions[current_question].options.length;i++){
+        opt+= '<li><label><input name="my_answer" type="radio" value="'+questions[current_question].options[i]+'">'+questions[current_question].options[i]+'</label></li>';
+    }
+    opt+= '</ul><br><center><button class="next-btn" onclick="check_answer('+current_question+')">Next</button></center>';
+    q_div.innerHTML = qst + opt;
+}
+
+show_question(current_question);
+
+function check_answer(current_question){
+    var answer = false;
+    var user_selected = false;
+    var your_answer = document.getElementsByName('my_answer');
+    var actual_answer = questions[current_question].answer;
+    for (i=0;i<your_answer.length;i++){
+        if (your_answer[i].checked){
+            user_selected = true;
+            my_answer = your_answer[i].value;
+            if (actual_answer == my_answer){
+                answer = true;
+            }
+        }
+    }
+
+    if (user_selected == false){
+        alert('Please select one answer.');
         return false;
     }
-    return true;
-}    
 
-
-
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-
-{/*// Click on a close button to hide the current list item*/}
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-
-{/*// Add a "checked" symbol when clicking on a list item*/}
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  console.log('clicked on:'+ev.target.tagName);
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
-{/*// Create a new list item when clicking on the "Add" button*/}
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+    current_question+=1;
+    if (answer) {                
+        score +=10;
+    } else {
+        wrong +=1;
     }
-  }
+
+    if (current_question < questions.length){
+        show_question(current_question);
+    } else  {
+        showResult();
+    }
+    var q_w = document.getElementById('wrong').innerHTML = wrong;
+    var q_score = document.getElementById('score').innerHTML = score;
 }
+
+function showResult(){
+    q_div.innerHTML = '<center><h1>Test Completed. Your total score is :'+score+'</h1><p>With '+wrong+' wrong answers</p><center>';
+}
+
+
+
+
+
+
+
+
+
+
+
